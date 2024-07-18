cbeb27603d1df53559033c6bd35c600b8e232d29
arelice <141432992+arelice@users.noreply.github.com>
2024-07-18 16:57:39 +0800

    Update relay_utils.go

--- relay/common/relay_utils.go
+++ relay/common/relay_utils.go
@@ -6,48 +6,11 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
-	"net/http"
 	"one-api/common"
-	"one-api/dto"
 	"strings"
 )
 
 var StopFinishReason = "stop"
-
-func RelayErrorHandler(resp *http.Response) *dto.OpenAIErrorWithStatusCode {
-	if resp == nil {
-		return &dto.OpenAIErrorWithStatusCode{
-			StatusCode: 500,
-			Error: dto.OpenAIError{
-				Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-				Type:    "model_unavailable",
-				Code:    "model_unavailable",
-			},
-		}
-	}
-
-	openAIError := &dto.OpenAIErrorWithStatusCode{
-		StatusCode: resp.StatusCode,
-		Error: dto.OpenAIError{
-			Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
-			Type:    "model_unavailable",
-			Code:    "model_unavailable",
-		},
-	}
-
-	// 读取上游响应体（可选，如果需要记录上游错误信息）
-	_, err := io.ReadAll(resp.Body)
-	if err != nil {
-		// 不记录错误
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		// 不记录错误
-	}
-
-	return openAIError
-}
 
 func GetFullRequestURL(baseURL string, requestURL string, channelType int) string {
 	fullRequestURL := fmt.Sprintf("%s%s", baseURL, requestURL)
